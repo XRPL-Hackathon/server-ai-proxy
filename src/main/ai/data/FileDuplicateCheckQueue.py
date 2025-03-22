cf73aab706e3af64ba68bf448e0345e555ec7bf5
Coldot <41678750+Coldot@users.noreply.github.com>
2025-03-22 18:19:30 +0900

    Feat: 파일 중복 검사 큐의 메시지 전송 메서드에 사용자 ID 및 요청 ID 매개변수 추가

--- src/main/ai/data/FileDuplicateCheckQueue.py
+++ src/main/ai/data/FileDuplicateCheckQueue.py
@@ -3,11 +3,16 @@ class FileDuplicateCheckQueue:
         self.sqs_client = sqs_client
         self.queue_url = queue_url
     
-    def send_message(self, message: str):
+    def send_message(self, message: str, user_id: str, request_id: str):
         """SQS 큐에 메시지를 전송합니다."""
+        print(message)
         response = self.sqs_client.send_message(
             QueueUrl=self.queue_url,
-            MessageBody=message
+            MessageGroupId=str(user_id),
+                MessageDeduplicationId=str(request_id),
+            MessageBody=message,
+
         )
+        
         
         return response 
