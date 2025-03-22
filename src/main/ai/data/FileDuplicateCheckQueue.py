8091e2e238d3fb34b36bdfc4ef26b9bb0cf6d7e5
Coldot <41678750+Coldot@users.noreply.github.com>
2025-03-22 18:27:12 +0900

    Feat: 파일 중복 검사 큐의 메시지 전송 메서드에 S3 버킷 및 키 매개변수 추가 및 메시지 형식 JSON으로 변경

--- src/main/ai/data/FileDuplicateCheckQueue.py
+++ src/main/ai/data/FileDuplicateCheckQueue.py
@@ -1,18 +1,27 @@
+import json
+
 class FileDuplicateCheckQueue:
     def __init__(self, sqs_client, queue_url):
         self.sqs_client = sqs_client
         self.queue_url = queue_url
     
-    def send_message(self, message: str, user_id: str, request_id: str):
+    def send_message(self, request_id: str, user_id: str, s3_bucket: str, s3_key: str):
         """SQS 큐에 메시지를 전송합니다."""
-        print(message)
+        message_body = {
+            'request_type': 'file_duplicate_check_embedding_file',
+            'request_id': request_id,
+            'user_id': user_id,
+            'payload': {
+                's3_bucket': s3_bucket,
+                's3_key': s3_key
+            }
+        }
+        
         response = self.sqs_client.send_message(
             QueueUrl=self.queue_url,
             MessageGroupId=str(user_id),
-                MessageDeduplicationId=str(request_id),
-            MessageBody=message,
-
+            MessageDeduplicationId=str(request_id),
+            MessageBody=json.dumps(message_body),
         )
-        
         
         return response 
